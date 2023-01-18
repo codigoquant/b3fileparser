2428385861a039ef6cf31a35a42ee050c8004b90
CódigoQuant <64567849+codigoquant@users.noreply.github.com>
2023-01-18 18:04:03 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 
 setup(
-  name='b3fileparser',
-  version='0.1',
-  description='A Python parser of B3 historical files',
-  url='#',
-  author='codigoquant',
-  author_email='codigoquant@gmail.com',
-  license='MIT',
-  packages=['b3fileparser'],
-  zip_safe=False
+    name='b3fileparser',
+    version='0.1',
+    description='A Python parser of B3 historical files',
+    url='https://github.com/codigoquant/b3fileparser',
+    author='codigoquant',
+    author_email='codigoquant@gmail.com',
+    license='MIT',
+    packages=['b3fileparser'],
+    zip_safe=False
 )
