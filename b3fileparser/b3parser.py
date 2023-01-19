1675ddd34871291cccffdb7d7fcb008398d6aa5e
Código Quant <codigoquant@gmail.com>
2023-01-18 22:16:29 -0300

    ..

--- b3fileparser/b3parser.py
+++ b3fileparser/b3parser.py
@@ -9,7 +9,7 @@ def read_b3_file(file_name):
     columns = []
     size_fields = []
 
-    for col, info in b3_meta_data.META_DATA.items():   
+    for col, info in META_DATA.items():   
         columns.append(info['name'])
         size_fields.append(info['size'])
 
@@ -26,10 +26,10 @@ def read_b3_file(file_name):
         if col in ["CODIGO_BDI", "PRAZO_EM_DIAS_DO_MERCADO_A_TERMO","NUMERO_DE_DISTRIBUICAO", "FATOR_DE_COTACAO", 'INDICADOR_DE_CORRECAO_DE_PRECOS']:
             b3_data[col] = b3_data[col].fillna(-1).astype(int)
         if col == "TIPO_DE_MERCADO":
-            b3_data[col] = b3_data[col].apply(lambda x: b3_meta_data.MARKETS[x])        
+            b3_data[col] = b3_data[col].apply(lambda x: MARKETS[x])        
         if col == "INDICADOR_DE_CORRECAO_DE_PRECOS":
-            b3_data[col] = b3_data[col].apply(lambda x: b3_meta_data.INDOPC[x]) 
+            b3_data[col] = b3_data[col].apply(lambda x: INDOPC[x]) 
         if col == "CODIGO_BDI":
-            b3_data[col] = b3_data[col].apply(lambda x: b3_meta_data.CODBDI[x])
+            b3_data[col] = b3_data[col].apply(lambda x: CODBDI[x])
     
     return b3_data
