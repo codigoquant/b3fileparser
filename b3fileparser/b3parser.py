87248d6f5f386d5f9fc537352f7655bf41840be8
CódigoQuant <64567849+codigoquant@users.noreply.github.com>
2024-03-07 14:18:30 +0000

    corrigindo erro de índices faltantes

--- b3fileparser/b3parser.py
+++ b3fileparser/b3parser.py
@@ -43,7 +43,7 @@ def read_b3_file(file_name):
             b3_data[col] = b3_data[col].fillna(-1).astype(int)
         if col == "TIPO_DE_MERCADO":
             b3_data[col] = b3_data[col].apply(
-                lambda x: b3_meta_data.MARKETS[x])
+                lambda x: b3_meta_data.MARKETS.get(x, "sem_registro"))
         if col == "INDICADOR_DE_CORRECAO_DE_PRECOS":
             b3_data[col] = b3_data[col].apply(lambda x: b3_meta_data.INDOPC.get(x, "sem_registro"))
         if col == "CODIGO_BDI":
--- tests/test_b3file.py
+++ tests/test_b3file.py
@@ -5,7 +5,7 @@ def get_path(name):
     return os.path.join(os.path.dirname(__file__), "sample_data", name)
 
 def test_zip_file():
-    dados_b3 = b3parser.read_b3_file(get_path('COTAHIST_A2023.ZIP'))    
+    dados_b3 = b3parser.read_b3_file(get_path('COTAHIST_A2024.ZIP'))    
     assert dados_b3 is not None
 
 def test_txt_file():
